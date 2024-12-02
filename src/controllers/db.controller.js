350a75a9a3e8c06e8ed0b9dbfe7d27368ada3681
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-12-02 20:58:06 -0300

    uuid for ID added

--- src/controllers/db.controller.js
+++ src/controllers/db.controller.js
@@ -2,24 +2,26 @@ import pool from '../db.js';  // Importa la conexión a la base de datos
 
 //Crear la tabla de usuarios de la base de datos
 const createUsersTable = async (req, res) => {
-  const query = `
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(100) UNIQUE NOT NULL,
-      password VARCHAR(100) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `;
-
-  try {
-    await pool.query(query);  // Ejecuta la consulta para crear la tabla
-    res.send('Tabla "users" creada exitosamente');
-  } catch (error) {
-    console.error('Error al crear la tabla "users":', error);
-    res.status(500).send('Hubo un error al crear la tabla');
-  }
-};
+    const query = `
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(100) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+  
+    try {
+      await pool.query(`CREATE EXTENSION IF NOT EXISTS "pgcrypto";`); // Asegura que la extensión esté habilitada
+      await pool.query(query);  // Ejecuta la consulta para crear la tabla
+      res.send('Tabla "users" creada exitosamente');
+    } catch (error) {
+      console.error('Error al crear la tabla "users":', error);
+      res.status(500).send('Hubo un error al crear la tabla');
+    }
+  };
+  
 
 ////////////////////////////////
 //Crear un usuario en la base de datos
